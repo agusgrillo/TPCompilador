e00b05541d10b9e3475597b7f449ed36f5b19a7a
ElPela0 <agrillo@alumnos.exa.unicen.edu.ar>
2026-09-15 17:27:15 -0300

    cambiamos TODO

--- Sintactico.py
+++ Sintactico.py
@@ -33,21 +33,69 @@ class Sintactico(sly.Parser):
         p.sentencias_declarativas.append(p.sentencia_declarativa)
         return p.sentencias_declarativas
 
-    
-    @_('SINGLEF lista_variables ";"')
+    @_('tipo lista_variables ";"')
     def sentencia_declarativa(self, p):
-        self.estructuras_detectadas.append(f"Línea {p.lineno}: Declaración de singlef para variables: {p.lista_variables}")
-        for var in p.lista_variables:
-            self.tabla_de_simbolos[var] = 0.0  # inicializamos las variables en 0.0
-        return f"Declaración de singlef: {p.lista_variables}"
 
-    @_('LONGINT lista_variables ";"')
+        for variable in p.lista_variables:
+
+            if variable in self.tabla_de_simbolos:
+                self.errores_sintacticos.append(
+                    f"Variable '{variable}' declarada más de una vez."
+                )
+
+            else:
+                if p.tipo == 'LONGINT':
+                    self.tabla_de_simbolos[variable] = {
+                        'tipo': 'LONGINT',
+                        'valor': 0
+                    }
+
+                elif p.tipo == 'SINGLEF':
+                    self.tabla_de_simbolos[variable] = {
+                        'tipo': 'SINGLEF',
+                        'valor': 0.0
+                    }
+
+        self.estructuras_detectadas.append(
+            f"Declaración {p.tipo}: {p.lista_variables}"
+        )
+
+        return (
+            'DECLARACION',
+            p.tipo,
+            p.lista_variables
+        )
+    #Funcion como declaracion
+    @_('sentencia_funcion')
     def sentencia_declarativa(self, p):
-        self.estructuras_detectadas.append(f"Línea {p.lineno}: Declaración de longint para variables: {p.lista_variables}")
-        for var in p.lista_variables:
-            self.tabla_de_simbolos[var] = 0  # inicializamos las variables en 0
-        return f"Declaración de longint: {p.lista_variables}"
+        return p.sentencia_function
 
+    # Enumeración / TYPEDEF
+    @_('TYPEDEF ID ASIGN_IGUAL "[" lista_valores "]" ";"')
+    def sentencia_declarativa(self, p):
+        self.tabla_de_simbolos[p.ID] = {
+            'tipo': 'TYPEDEF',
+            'valores': p.lista_valores
+        }
+        self.estructuras_detectadas.append(f"Línea {p.lineno}:TYPEDEF '{p.ID}'")
+        return (
+            'TYPEDEF',
+            p.ID,
+            p.lista_valores
+        )
+    #asignacion de tipo declarado del typedef
+    @_('ID lista_variables ";"')
+    def sentencia_declarativa(self, p):
+
+        self.estructuras_detectadas.append(
+            f"Declaración del tipo '{p.ID}': {p.lista_variables}"
+        )
+        return (
+            'DECLARACION_TIPO_USUARIO',
+            p.ID,
+            p.lista_variables
+        )
+    #Declaracion de variables separadas por comas
     @_('lista_variables "," ID')
     def lista_variables(self, p):
         lista_actual = p.lista_variables
@@ -57,58 +105,206 @@ class Sintactico(sly.Parser):
     @_('ID')
     def lista_variables(self, p):
         return [p.ID]
-    
+
+    #declaracion de clases
+    @_('sentencia_clase')
+    def sentencia_declarativa(self, p):
+        return p.sentencia_clase
+
+    #tipos
+    @_('LONGINT')
+    def tipo(self, p):
+        return 'LONGINT'
+    @_('SINGLEF')
+    def tipo(self, p):
+        return 'SINGLEF'
+
+    #-------------------------------------------------------------------
     #Fin de la parte declarativa
+    #-------------------------------------------------------------------
+    
     #Parte ejecutable
     @_('sentencias_ejecutables sentencia_ejecutable')
     def sentencias_ejecutables(self, p):
         p.sentencias_ejecutables.append(p.sentencia_ejecutable)
         return p.sentencias_ejecutables
+    @_('sentencia_ejecutable')
+    def sentencias_ejecutables(self, p):
+        return [p.sentencia_ejecutable]
+
+    #Asignacion
+    @_('asignacion ";"')
+    def sentencia_ejecutable(self,p):
+        return p.asignacion
+    #if
+    @_('sentencia_if')
+    def sentencia_ejecutable(self, p):
+        return p.sentencia_if
+    #repeat
+    @_('sentencia_repeat')
+    def sentencia_ejecutable(self, p):
+        return p.sentencia_repeat
+    #pout
+    @_('salida ";"')
+    def sentencia_ejecutable(self, p):
+        return p.salida
+    #llamado de funcion
+    @_('llamado_funcion ";"')
+    def sentencia_ejecutable(self, p):
+        return p.llamado_funcion
+    #tosf
+    @_('sentencia_conv ";"')
+    def sentencia_ejecutable(self, p):
+        return p.sentencia_conv
+    #retorno
+    @_('retorno')
+    def sentencia_ejecutable(self, p):
+        return p.retorno
+
+    #ASIGNACIONES
+
+    @_('referencia ASIGN expresion'
+       'referencia ASIGN_IGUAL expresion')
+    def asignacion(self,p):
+        if p[1] == ':=':
+            self.estructuras_detectadas.append(f"Línea {p.lineno}:Asignación ':=' sobre {p.referencia}")
+        elif p[1] == '=':
+            self.estructuras_detectadas.append(f"Línea {p.lineno}:Asignación '=' sobre {p.referencia}")
+        #si la variable ya esta asignada
+        if isinstance(p.referencia, str):
+            if p.referencia not in self.tabla_de_simbolos:
+                self.errores_sintacticos.append(f"Variable '{p.referencia}' no declarada" )
+            else:
+                self.tabla_de_simbolos[p.referencia]['valor'] = p.expresion
+        return('ASIGNACION',p.referencia,p.expresion)
+
+    #REFERENCIAS
+
+    @_('ID')
+    def referencia(self, p):
+        return p.ID
+
+    @_('referencia "." ID')
+    def referencia(self, p):
+        return ('REFERENCIA',p.referencia, p.ID)
+
+    #EXPRESIONES MATEMATICAS
+    @_('expresion MAS termino', 
+       'expresion MENOS termino')
+    def expresion(self, p):
+        return ('OP_BINARIA', p[1], p.expresion, p.termino)
+
+    @_('termino')
+    def expresion(self, p):
+        return p.termino
+
+    @_('termino MULT factor', 
+       'termino DIV factor')
+    def termino(self, p):
+        return ('OP_BINARIA', p[1], p.termino, p.factor)
+
+    @_('factor')
+    def termino(self, p):
+        return p.factor
+
+    @_('numero','FLOAT','referencia','asign_expresion','llamado_funcion','sentencia_conv')
+    def factor(self, p):
+        return(p[0])
     
+    @_('MENOS NUMBER %prec UMINUS')
+    def factor(self, p):
+        return ('NEGATIVO',-p.NUMBER)
 
-    @_('ID ASIGN  expr',
-       'ID ASIGN_IGUAL expr')
-    def sentencia_ejecutable(self, p):
-        self.estructuras_detectadas.append(f"Línea {p.lineno}: Asignación")
-        self.tabla_de_simbolos[p.ID] = p.expr
-        return f"{p.ID} = {p.expr}"
 
-    @_('expr')
-    def sentencia_ejecutable(self, p):
-        self.estructuras_detectadas.append(f"Línea {p.lineno}: Expresión")
-        return p.expr
+    @_('ID ASIGN_IGUAL "(" expresion_estricta ")"')
+    def asign_expresion(self,p):
+        return ('ASIGNACION_EN_EXPRESION', p.ID, p.expresion_estricta)
 
-    @_('expr MAS expr')
-    def expr(self, p):
-        return p.expr0 + p.expr1
+    @_('expresion_estricta MAS termino_estricto',
+       'expresion_estricta MENOS termino_estricto')
+    def expresion_estricta (self,p):
+        return('OP_BINARIA',p[1],p.expresion_estricta,p.termino_estricto)
 
-    @_('expr MENOS expr')
-    def expr(self, p):
-        return p.expr0 - p.expr1
+    @_('termino_estricto')
+    def expresion_estricta(self,p):
+        return(p.termino_estricto)
 
-    @_('expr MULT expr')
-    def expr(self, p):
-        return p.expr0 * p.expr1
+    @_('termino_estricto MULT factor_estricto',
+       'termino_estricto DIV factor_estricto')
+    def termino_estricto (self , p):
+        if p[1] == 'DIV':
+            if p[2] == 0:
+                self.errores_sintacticos.append(f"Línea {p.lineno}: Error: División por cero.")
+                raise ZeroDivisionError("Error: División por cero.")
+        return('OP_BINARIA',p[1],p.termino_estricto,p.factor_estricto)
 
-    @_('expr DIV expr')
-    def expr(self, p):
-        if p.expr1 == 0:
-            self.errores_sintacticos.append(f"Línea {p.lineno}: Error: División por cero.")
-            raise ZeroDivisionError("Error: División por cero.")
-        return p.expr0 / p.expr1
+    @_('factor_estricto')
+    def termino_estricto (self , p):
+        return(p.factor_estricto)
 
-    @_('MENOS expr %prec UMINUS')
-    def expr(self, p):
-        return -p.expr
+    @_('numero','FLOAT','ID','llamado_funcion','sentencia_conv')
+    def factor_estricto(self,p):
+        return p[0]
+
+    @_('MENOS NUMBER %prec UMINUS')
+    def factor_estricto(self, p):
+        return ('NEGATIVO',-p.NUMBER)
 
     @_('NUMBER')
-    def expr(self, p):
-        numero = int(p.NUMBER)
-        if numero < -2147483648 or numero > 2147483647:
-            raise ValueError(f"Error: Constante entera fuera del rango permitido: {numero}")
-        else:
-            return int(p.NUMBER)
+    def numero(self, p):
+        if p.NUMBER > 2147483647:
+            raise ValueError(f"Constante entera positiva fuera de rango: {p.NUMBER}")
+        return p.NUMBER
 
-    @_('FLOAT')
-    def expr(self, p):
-        return float(p.FLOAT)
+    #Estructura Condicion
+    @_('expresion comparador expresion')
+    def condicion(self,p):
+        return('CONDICION',p[1],p[0],p[2])
+
+    #comparadores
+    @_('MAYOR', 'MENOR', 'MAYORIGUAL', 'MENORIGUAL', 'IGUAL', 'DIFERENTE')
+    def comparador (self, p):
+        return p[0]
+
+    #bloque de control
+    @_('sentencia_ejecutable')
+    def bloque_control(self, p):
+        return [p.sentencia_ejecutable]
+
+    @_('BEGIN sentencias_ejecutables END')
+    def bloque_control(self, p):
+        return p.sentencias_ejecutables
+
+    # Estructura IF
+    @_('IF "(" condicion ")" bloque_control END_IF ";"')
+    def sentencia_if (self,p):
+        self.estructuras_detectadas.append(f"Línea {p.lineno}:Estructura IF")
+        return ('IF', p.condicion, p.bloque_control)
+    #IF-ELSE
+    @_('IF "(" condicion ")" bloque_control ELSE bloque_control END_IF ";"')
+    def sentencia_if (self,p):
+        self.estructuras_detectadas.append(f"Línea {p.lineno}:Estructura IF-ELSE")
+        return ('IF-ELSE', p.condicion, p.bloque_control)
+
+    #REPEAT UNTIL
+    @_('REPEAT bloque_control UNTIL "(" condicion ")" ";"')
+    def sentencia_repeat(self, p):
+        self.estructuras_detectadas.append(f"Línea {p.lineno}: Sentencia REPEAT")
+        return ('REPEAT', p.bloque_control, p.condicion)
+
+    #FUNCIONES
+    @_('tipo FUNCTION ID "(" parametros_formales ")" sentencias_declarativas BEGIN sentencias_ejecutables END ";"')
+    def declaracion_funcion(self, p):
+        return ('FUNCION', p.tipo, p.ID, p.parametros_formales, p.sentencias_declarativas, p.sentencias_ejecutables)
+
+    #para mas de una variable
+    @_('tipo ID')
+    def parametros_formales(self, p):
+        return [(p.tipo, p.ID)]
+    @_('parametros_formales "," tipo ID')
+    def parametros_formales(self, p):
+        p.parametros_formales.append((p.tipo, p.ID))
+        return p.parametros_formales
+    @_('RET "(" expresion ")" ";"')
+    def retorno(self, p):
+        return ('RETORNO', p.expresion)
